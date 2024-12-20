3f80ae95a4baeb1a67159aa245128d5473d27c9d
Asmi Vats <asmivats15@gmail.com>
2024-12-20 21:49:56 +0530

    added index

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
