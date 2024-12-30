9fc3f0d309e7052a71f1da6bf943c8627c4746cf
rishav-bhardwaz <rishav76000@gmail.com>
2024-12-30 21:03:09 +0530

    Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Landingpages } from "./pages/Landingpages";
 import LoginPage from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./admincomponents/Dashboard";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "./Sidebar"
+import Sidebar from "../admincomponents/Sidebar"
 const Dashboard = () => {
   return (
     <Sidebar />
