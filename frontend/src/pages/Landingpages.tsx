9c937ac4f2faf32939324a27e0d5c8c872e12c68
rishav-bhardwaz <rishav76000@gmail.com>
2025-01-03 21:38:52 +0530

    Logout Logic added

--- frontend/src/pages/Landingpages.tsx
+++ frontend/src/pages/Landingpages.tsx
@@ -5,9 +5,13 @@ import Bento from "../components/Landing/Bento"
 import Team from "../components/Landing/Team"
 import Footer from "../components/Common/Footer"
 export const Landingpages = () => {
+
+  const handleLogout = () => {
+    console.log("User logged out");
+  };
   return (
     <>
-    <Header />
+    <Header onLogout={handleLogout}/>
     <HeroSection />
     <Job />
     <Bento />
